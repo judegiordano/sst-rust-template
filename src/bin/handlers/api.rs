d3c574fc5584c128241fd4cd901b2d38de22dc55
judegiordano <judegiordano@gmail.com>
2023-09-19 14:48:48 -0400

    Simplify Factory

--- src/bin/handlers/api.rs
+++ src/bin/handlers/api.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 use lambda_web::{
     actix_web::{web::scope, App},
-    run_actix_on_lambda,
+    run_actix_on_lambda as run,
 };
 use sst_example::controllers::routes;
 
 #[tokio::main]
 pub async fn main() -> Result<(), lambda_http::Error> {
-    let factory = move || App::new().service(scope("/api").configure(routes));
-    run_actix_on_lambda(factory).await
+    run(move || App::new().service(scope("/api").configure(routes))).await
 }
